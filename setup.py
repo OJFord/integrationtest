4e4b4aa2a6b38ac59c440e4521b3cb4a2d302739
Ollie Ford <me@ojford.com>
2016-06-02 22:22:04 +0100

    initialises project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='integrationtest',
+    version='0.1dev',
+    author='Ollie Ford <me@ojford.com>'
+    packages=['integrationtest'],
+    long_description=open('README.md').read()
+)
