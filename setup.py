000911d967e5cf334f206209bdbd8ad7a48aab31
Ollie Ford <me@ojford.com>
2016-06-03 23:15:49 +0100

    patches setup - bumps v0.1.1

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name='integrationtest',
-    version='0.1',
-    author='Ollie Ford <me@ojford.com>'
+    version='0.1.1',
+    author='Ollie Ford <me@ojford.com>',
     packages=['integrationtest'],
     long_description=open('README.md').read()
 )
