c8e060b5fc8f617e11fb3e5bef9dec5f1c9d0a9e
Ollie Ford <me@ojford.com>
2016-06-05 16:36:29 +0100

    bumps version to v0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='integrationtest',
-    version='0.1.1',
+    version='0.2',
     author='Ollie Ford <me@ojford.com>',
     packages=['integrationtest'],
     long_description=open('README.md').read()
